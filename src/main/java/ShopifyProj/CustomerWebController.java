8e1923550c061c1cc03851c84adcb00f67fc6cab
Ryan <ryanzheng@cmail.carleton.ca>
2020-03-05 12:41:00 -0500

    Updated UML and renamed web controller

--- src/main/java/ShopifyProj/CustomerWebController.java
+++ src/main/java/ShopifyProj/CustomerWebController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CustomerController {
+public class CustomerWebController {
     @Autowired
     private CustomerRepository customerRepository;
 
