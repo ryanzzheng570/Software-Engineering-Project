476606f57550cb2fd16b7c38ccabb0560da0e1e9
Calvin <calvinsoong38@gmail.com>
2020-03-09 17:11:36 -0400

    Add default message and link to shop

--- src/main/resources/static/js/searchController.js
+++ src/main/resources/static/js/searchController.js
@@ -8,14 +8,21 @@ $(document).ready(function() {
             dataType: "json"
         }).then(function (data) {
             $("#results").empty();
-            for (let i = 0; i < data.length; i++) {
-                let tags = "";
-                for(let j = 0; j < data[i].tags.length; j++) {
-                    tags += data[i].tags[j].tagName;
-                }
-                let str = "Shop ID: " + data[i].id + ", Shop Name: " + data[i].shopName + ", Shop Tags: " + tags;
+            if (data.length == 0) {
+                $("#results").append('<p>Sorry, no shops were found</p>');
+            }
+            else {
+                for (let i = 0; i < data.length; i++) {
+                    let tags = "";
+                    for(let j = 0; j < data[i].tags.length; j++) {
+                        tags += data[i].tags[j].tagName;
+                    }
+                    let str = "Shop ID: " + data[i].id + ", Shop Name: " + data[i].shopName + ", Shop Tags: " + tags;
 
-                $("#results").append(`<p>${str}</p>`);
+                    let shopUrl = `/goToShop?shopId=${data[i].id}`;
+                    let button = `<input type="button" value="View Shop as Customer" onclick="location.href='${shopUrl}'"></input>`;
+                    $("#results").append(`<p style="display: inline-block">${str}</p>${button}</br>`);
+                }
             }
         })
     })
