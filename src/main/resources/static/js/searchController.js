b4bfa3e07a8a73db22b2a4c248134eedf8fe97aa
Calvin <calvinsoong38@gmail.com>
2020-03-10 23:08:49 -0400

    Change search result to table format

--- src/main/resources/static/js/searchController.js
+++ src/main/resources/static/js/searchController.js
@@ -12,19 +12,20 @@ $(document).ready(function() {
                 $("#results").append('<p>Sorry, no shops were found</p>');
             }
             else {
+                $("#results").append('<table id="results-table"><tr><th>Shop ID</th><th>Shop Name</th><th>Shop Tags</th><th></th></tr>');
                 for (let i = 0; i < data.length; i++) {
-                    let tags = "";
+                    let tags = "[";
                     for(let j = 0; j < data[i].tags.length; j++) {
                         tags += data[i].tags[j].tagName;
-                        if (j != data[i].tags.length -1) {
+                        if (j != data[i].tags.length - 1) {
                             tags += ", ";
                         }
                     }
-                    let str = "Shop ID: " + data[i].id + ", Shop Name: " + data[i].shopName + ", Shop Tags: " + tags;
-
+                    tags += "]";
                     let shopUrl = `/goToShop?shopId=${data[i].id}`;
                     let button = `<input type="button" value="View Shop as Customer" style="margin-left: 20px" onclick="location.href='${shopUrl}'"></input>`;
-                    $("#results").append(`<p style="display: inline-block">${str}</p>${button}</br>`);
+                    let tr = "<tr><td>" + data[i].id + "</td><td>" + data[i].shopName + "</td><td>" + tags + "</td><td>" + button + "</td></tr>";
+                    $("#results-table").append(tr);
                 }
             }
         })
