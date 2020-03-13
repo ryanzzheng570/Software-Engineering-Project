86ce424819cd9cf34f91591682674abffcc7a6b4
BramSrna <bramsrna@cmail.carleton.ca>
2020-03-13 18:04:39 -0400

    Create sidebar

---
+++ src/main/resources/static/js/SidebarController.js
@@ -0,0 +1,37 @@
+function addSidebar(){
+    $("head").prepend("<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/SidebarStyles.css\">");
+    $("body").wrapInner("<div class='main' />");
+
+    var sidebarDiv = document.createElement("div");
+    sidebarDiv.className = "sidenav";
+
+    var searchLink = document.createElement("a");
+    var searchText = document.createTextNode("Search");
+    searchLink.href = "/search";
+    searchLink.appendChild(searchText);
+    sidebarDiv.appendChild(searchLink);
+
+    var homeLink = document.createElement("a");
+    var homeText = document.createTextNode("Home");
+    homeLink.href = "/";
+    homeLink.appendChild(homeText);
+    sidebarDiv.appendChild(homeLink);
+
+    var merchantMenuLink = document.createElement("a");
+    var merchantMenuText = document.createTextNode("Merchant Menu");
+    merchantMenuLink.href = "/goToMerchantMenuPage";
+    merchantMenuLink.appendChild(merchantMenuText);
+    sidebarDiv.appendChild(merchantMenuLink);
+
+    var createShopLink = document.createElement("a");
+    var createShopText = document.createTextNode("Create Shop");
+    createShopLink.href = "/goToAddShopPage";
+    createShopLink.appendChild(createShopText);
+    sidebarDiv.appendChild(createShopLink);
+
+    $("body").prepend(sidebarDiv);
+}
+
+$(document).ready(function() {
+    addSidebar();
+})
