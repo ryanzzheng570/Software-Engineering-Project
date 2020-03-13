ba7eb70d5a5f4959158c54b008b943062349a1b9
Calvin <calvinsoong@cmail.carleton.ca>
2020-03-13 16:16:41 -0400

    WIP checkout page

--- src/main/java/ShopifyProj/Controller/CheckoutController.java
+++ src/main/java/ShopifyProj/Controller/CheckoutController.java
@@ -1,19 +1,49 @@
 package ShopifyProj.Controller;
 
+import ShopifyProj.Model.Customer;
+import ShopifyProj.Model.Item;
 import ShopifyProj.Model.Shop;
+import ShopifyProj.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+import java.util.Set;
+
 
 @Controller
 public class CheckoutController {
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @PostMapping("/addToCart")
-    public String viewAddShopPage(Model model) {
+    public String addToCart(Model model) {
+//        Optional<Customer> customer = customerRepository.findById(customerId);
+//        Set<Item> customerCart;
+//
+//        if(customer.isPresent()){
+//            customerCart = customer.getCart();
+//            System.out.println(customerCart);
+//        }else {
+//            System.out.println(String.format("No customer found with ID %d", customerId));
+//        }
 
         return "CheckoutPage";
     }
+
+    @PostMapping("/checkout")
+    public String checkout(@RequestParam(value = "paymentName") String paymentName,
+                           @RequestParam(value = "ccNum") int ccNum,
+                           Model model) {
+        System.out.println("in checkout");
+        System.out.println("paymentName " + paymentName);
+        System.out.println("ccNum " + ccNum);
+
+        return "CheckoutPage";  //TODO
+    }
+
 }
--- src/main/resources/static/js/AddToCartController.js
+++ src/main/resources/static/js/AddToCartController.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-    $("#cartForm").submit(function (e) {
-        e.preventDefault();
-        console.log($("#shopItems")[0].rows);
-        let tableRows = $("#shopItems")[0].rows;
-        // for (let i=1;i<tableRows.length; i++){
-        //     console.log("name", tableRows[i])
-        // };
-        let itemNames = document.getElementsByName('itemName');
-        let itemInventory = document.getElementsByName('itemInventory');
-
-
-
-        $.ajax({
-            url: "/addToCart?" + $("#cartForm").serialize(),
-            type: "POST",
-            dataType: "json"
-        }).then(function (data) {});
-    })
+    // $("#cartForm").submit(function (e) {
+    //     e.preventDefault();
+    //     console.log($("#shopItems")[0].rows);
+    //     let tableRows = $("#shopItems")[0].rows;
+    //     // for (let i=1;i<tableRows.length; i++){
+    //     //     console.log("name", tableRows[i])
+    //     // };
+    //     let itemNames = document.getElementsByName('itemName');
+    //     let itemInventory = document.getElementsByName('itemInventory');
+    //
+    //
+    //
+    //     $.ajax({
+    //         url: "/addToCart?" + $("#cartForm").serialize(),
+    //         type: "POST",
+    //         dataType: "json"
+    //     }).then(function (data) {});
+    // })
 })
 
