25dae93a10f033daa6ee2471028fe88de03c4d65
Haseeb <haseebakhan@cmail.carleton.ca>
2020-03-14 13:18:20 -0400

    Deployed function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
+// https://firebase.google.com/docs/functions/write-firebase-functions
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.exampleCloudFunction = functions.https.onRequest((request, response) => {
+    console.log("\n---------REACHED CLOUD FUNCTION-------------\n")
+    response.send({returnData: "Hello from Firebase!"});
+});
