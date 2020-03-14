283fc03a2ec6908f08d4ea5abe528ddd334b54c3
Haseeb <haseebakhan@cmail.carleton.ca>
2020-03-14 14:54:33 -0400

    Setup of firebase compelte

--- src/main/resources/static/js/CloudServiceController.js
+++ src/main/resources/static/js/CloudServiceController.js
@@ -1,31 +1,40 @@
-$.getScript('https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js', () => {
-$.getScript('https://www.gstatic.com/firebasejs/7.11.0/firebase-auth.js', () => {
-$.getScript('https://www.gstatic.com/firebasejs/7.11.0/firebase-functions.js', () => {
+async function ExampleCloudFunctionCall(someData) {
+    var response = await callCloudFunction("exampleCloudFunction", {data: someData});
+    console.log(response.data)
+}
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyA8CjyW4zT7jL46sG4rEOcCON7SNsDN_do",
-        authDomain: "engineeringlabproject.firebaseapp.com",
-        databaseURL: "https://engineeringlabproject.firebaseio.com",
-        projectId: "engineeringlabproject",
-        storageBucket: "engineeringlabproject.appspot.com",
-        messagingSenderId: "523387019136",
-        appId: "1:523387019136:web:e414700e2e6bb35e372b65",
-        measurementId: "G-5XBXE4QJLC"
-    };
+// !--- PLACE ALL CLOUD SERVICE CALLS ABOVE HERE --- SHOULD NOT HAVE TO CHANGE ANYTHING BELOW ---!
 
-    firebase.initializeApp(firebaseConfig);
-    firebase.functions();
+document.writeln('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>');
+document.writeln('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/7.11.0/firebase-auth.js"></script>');
+document.writeln('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/7.11.0/firebase-functions.js"></script>');
 
-
-    var exampleCloudFunction = firebase.functions().httpsCallable('exampleCloudFunction');
-    exampleCloudFunction({text: "Making a call to the Cloud Function"}).then((result) => {
-      console.log(result.data.text);
-    }).catch((error) => {
-      var code = error.code;
-      var message = error.message;
-      var details = error.details;
-      console.log("\nCode: " + code + "\nMessage: " + message + "\nDetails: " + details);
-
+function callCloudFunction(functionName, inputData) {
+    var callFunction = firebase.functions().httpsCallable(functionName);
+    return new Promise((resolve, reject) => {
+        callFunction(inputData).then((result) => {
+                resolve(result);
+        }).catch((error) => {
+          var code = error.code;
+          var message = error.message;
+          var details = error.details;
+          console.log("\nCode: " + code + "\nMessage: " + message + "\nDetails: " + details);
+          reject(error)
+        })
     });
+}
 
-});});});
+$(document).ready(() => {
+        var firebaseConfig = {
+            apiKey: "AIzaSyA8CjyW4zT7jL46sG4rEOcCON7SNsDN_do",
+            authDomain: "engineeringlabproject.firebaseapp.com",
+            databaseURL: "https://engineeringlabproject.firebaseio.com",
+            projectId: "engineeringlabproject",
+            storageBucket: "engineeringlabproject.appspot.com",
+            messagingSenderId: "523387019136",
+            appId: "1:523387019136:web:e414700e2e6bb35e372b65",
+            measurementId: "G-5XBXE4QJLC"
+        };
+        firebase.initializeApp(firebaseConfig);
+        firebase.functions();
+});
